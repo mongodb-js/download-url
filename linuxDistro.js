40c3d7f5a8b863bf71b2b77cd879c7562adb7fad
Dan Aprahamian <dan.aprahamian@gmail.com>
2019-07-19 14:43:33 -0400

    fix(ubuntu): add support for ubuntu distros for v4

---
+++ linuxDistro.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var exec = require('child_process').execSync;
+
+exports.lsbReleaseInfo = lsbReleaseInfo;
+exports.getDistro = getDistro;
+
+function getDistro() {
+    var distroInfo = lsbReleaseInfo();
+    switch (distroInfo.distroId) {
+        case 'Ubuntu':
+            var version = distroInfo.distroVersion.replace('.', '') || '1604';
+            return 'ubuntu' + version;
+        default:
+            return '';
+    }
+}
+
+function lsbReleaseInfo() {
+    var distroId = exec('lsb_release -si', {encoding: 'utf8' });
+    var distroVersion = exec('lsb_release -sr', { encoding: 'utf8' });
+
+    var ret = {
+        distroId: distroId.trim(),
+        distroVersion: distroVersion.trim()
+    };
+
+    return ret;
+}
